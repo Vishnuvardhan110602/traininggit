c905ee41765b250a123af68e3158b89f664e43aa
Vishnuvardhan110602 <nvishnuvardhanrv@gmail.com>
2025-01-29 19:39:12 +0530

    json

--- Day14/api.js
+++ Day14/api.js
@@ -1,11 +1,30 @@
-let obj={
-    name :"kumar",
-    id : 1,
-    role : "developer"
-}
-console.log(obj);
+// let obj={
+//     name :"kumar",
+//     id : 1,
+//     role : "developer"
+// }
+// console.log(obj);
 
-let obj1 = JSON.stringify(obj)
-console.log(obj1);
-let obj2 = JSON.parse(obj1)
-console.log(obj2);
+// let obj1 = JSON.stringify(obj)
+// console.log(obj1);
+// let obj2 = JSON.parse(obj1)
+// console.log(obj2);
+let a = window.fetch()
+console.log(a);
+
+window.fetch("data.json")
+.then((data) => data.json())
+.then((res) => {
+    console.log(res);
+
+
+res.map((x) => {
+    console.log(x);
+    console.log(x.name);
+    console.log(x.id);
+    document.body.innerHTML += <h2>${x.name}</h2>
+    document.body.innerHTML += <h2>${x.id}</h2>
+    
+})
+    
+})
